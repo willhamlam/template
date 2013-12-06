d08a66530c2f66dc8e9961f59d37783fae665a31
will <iamhandian@gmail.com>
2013-12-06 16:28:55 +0800

    add template

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/base.css">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/pure/pure.min.css">
+</head>
+<body>
+	hello world;
+	<script src="js/jquery.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
